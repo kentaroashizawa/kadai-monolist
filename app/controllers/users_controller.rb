a781bfcc5bc4ce91e79858a061f224758fd1aa80
kentaro.ashizawa <ken.aszw@gmail.com>
2018-08-04 12:14:18 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
   def show
     @user = User.find(params[:id])
   end
@@ -18,10 +19,10 @@ class UsersController < ApplicationController
       render :new
     end
   end
-end
-
+  
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+end
