b2fd0c2d9679e1150fc54f91f12f51190fc1f63c
kentaro.ashizawa <ken.aszw@gmail.com>
2018-08-05 11:27:03 +0000

    have

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @items = @user.items.uniq
     @count_want = @user.want_items.count
+    
+    @user = User.find(params[:id])
+    @items = @user.items.uniq
+
+    @user = User.find(params[:id])
+    @items = @user.items.uniq
+    @count_have = @user.have_items.count
   end
 
   def new
