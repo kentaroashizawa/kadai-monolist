d73283a6139c976aeb17c76369a663d06fbe95d7
kentaro.ashizawa <ken.aszw@gmail.com>
2018-08-07 01:20:25 +0000

    post

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -14,9 +14,7 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
-    end
-    # Have 関係として保存
-    if params[:type] == 'Have'
+    elsif params[:type] == 'Have'
       current_user.have(@item)
       flash[:success] = '商品を Have しました。'
     end
